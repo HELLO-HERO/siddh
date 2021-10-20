0c7b011f035a2914cc5363d29560e00cdc930fdf
SIDDHARTH <91492337+HELLO-HERO@users.noreply.github.com>
2021-10-20 23:00:49 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,6 @@
+var first_name = Siddharth;
+console.log (first_name);
+var last_name = Saimukundhan;
+console.log (last_name);
+var full_name = first_name.concat (last_name);
+console.log (full_name)
